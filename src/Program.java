6f571975e22a5d5541df6a741eaf178fc8a6dacb
Sergey Malyshev <malyshevs_v@mail.ru>
2020-09-28 13:46:25 +0300

    modified

--- src/Program.java
+++ src/Program.java
@@ -1,47 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Program {
-    public static void main(String[] args) throws Exception {
+public class Program{
+    public static void main(String[] args) throws IOException {
 
-        //Чтение из файла
         FileReader fr = new FileReader("c:/test/login.txt");
         FileReader fr1 = new FileReader("c:/test/Pass.txt");
-        //Создание каталога
+
         File dir = new File("c:/test/Registration");
-        System.out.println(dir.mkdir()? "Success!":"Faled");
-        //Создание файла
+        System.out.println(dir.mkdir()?"Success":"Failed");
         File file = new File("c:/test/Registration/reg.txt");
-        System.out.println(file.createNewFile()? "Success!":"Faled");
+        file.createNewFile();
 
         FileWriter fw = new FileWriter(file);
-        //Создание буфера
-        var readerLogin = new BufferedReader(fr);
-        var readerPass = new BufferedReader(fr1);
-        String strLogin;
-        String strPass;
-        //Создание массива
+
         List<String> login = new ArrayList<>();
         List<String> pass = new ArrayList<>();
         Map<String, String> reg = new HashMap<>();
 
-        while ((strLogin = readerLogin.readLine()) != null){
+        BufferedReader readerLogin = new BufferedReader(fr);
+        BufferedReader readerPass = new BufferedReader(fr1);
+        String strLogin;
+        String strPass;
+
+        while((strLogin = readerLogin.readLine()) != null){
+            strPass = readerPass.readLine();
             if(strLogin.isEmpty()) break;
             login.add(strLogin);
-        }
-
-        for (int i = 0; i < login.size(); i++) {
-            strPass = readerPass.readLine();
             pass.add(strPass);
         }
+
 
         for (int i = 0; i < login.size(); i++) {
             reg.put(login.get(i), pass.get(i));
         }
 
         for (Map.Entry x: reg.entrySet()) {
-            fw.write(String.format("Login: %15s\tPassword: %s\n",
-                    x.getKey().toString(), x.getValue()));
+            fw.write(x.getKey().toString() + x.getValue() + '\n');
         }
 
         fr.close();
@@ -49,5 +44,12 @@ public class Program {
         fw.close();
         readerLogin.close();
         readerPass.close();
+
+
+
+
+
+
+
     }
 }
